b965405d56719631ae3a8c0b28866319382f947f
Matthew <mmlak_0109@yahoo.com>
2021-04-28 23:54:49 +0900

    Turn into bottom tab navigation

--- components/HomeNav.js
+++ components/HomeNav.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Decks from './Decks';
+import AddDeck from './AddDeck';
+import Stats from './Stats';
+
+const HomeTabs = createBottomTabNavigator();
 
 const HomeNav = () => {
   return (
-    <View>
-      <Ionicons name="home" size={24} color="black" />
+    <HomeTabs.Navigator initialRouteName='Home'>
+      <HomeTabs.Screen name='Home' component={Decks} />
+      <HomeTabs.Screen name='Add Deck' component={AddDeck} />
+      <HomeTabs.Screen name='Stats' component={Stats} />
+      {/* <Ionicons name="home" size={24} color="black" />
       <MaterialCommunityIcons name="plus-circle" size={24} color="black" />
-      <Ionicons name="stats-chart" size={24} color="black" />
-    </View>
+      <Ionicons name="stats-chart" size={24} color="black" /> */}
+    </HomeTabs.Navigator>
   )
 }
 
