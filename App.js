401225b2fb5a302a11addcb9b33d0e70a5b50642
Matthew <mmlak_0109@yahoo.com>
2021-04-28 23:11:31 +0900

    Wrap code with NavigationContainer

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import Decks from './components/Decks';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Decks />
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Decks />
+      </NavigationContainer>
     </View>
   );
 }
