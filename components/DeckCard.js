ca145b9e6bf2560bc19b9c00717b36d8cade44ff
Matthew <mmlak_0109@yahoo.com>
2021-04-28 22:53:25 +0900

    Add stylings to pressable

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 const DeckCard = ({item}) => {
   return (
     <View>
-      <Pressable>
+      <Pressable style={styles.deckBtn}>
         <Text>{item.title}</Text>
-        <Text>{item.cardCount} Cards</Text>
-        <Feather name="chevron-right" size={24} color="black" />
+        <Text> ({item.cardCount} Cards)</Text>
+        <Feather name="chevron-right" size={24} color="blue" />
       </Pressable>
     </View>
   )
 }
 
 export default DeckCard
+
+const styles = StyleSheet.create({
+  deckBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    borderRadius: 6,
+    padding: 18,
+    marginHorizontal: 24,
+    marginBottom: 12,
+  }
+})
