40cef4f3790f8c02217add0aedbcd87dd1d131d4
Matthew <mmlak_0109@yahoo.com>
2021-04-29 23:49:15 +0900

    Create skeleton component

---
+++ components/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
+import HeaderBar from './HeaderBar';
+
+const Quiz = () => {
+  return (
+    <View>
+      <HeaderBar title='Quiz'/>
+      <Text>Quiz</Text>
+    </View>
+  )
+}
+
+export default Quiz
