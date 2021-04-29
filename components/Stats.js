b413f69fa68246c90011640308a97543c2d66c7b
Matthew <mmlak_0109@yahoo.com>
2021-04-29 14:10:48 +0900

    Add HeaderBar

--- components/Stats.js
+++ components/Stats.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
+import HeaderBar from './HeaderBar';
 
 const Stats = () => {
   return (
     <View>
+      <HeaderBar title='Stats' />
       <Text>Stats</Text>
     </View>
   )
