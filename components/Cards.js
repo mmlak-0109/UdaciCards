87121f85ff987adeb5f48045ecff6c1807f245c9
Matthew <mmlak_0109@yahoo.com>
2021-04-29 23:47:06 +0900

    Create skeleton component

---
+++ components/Cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Pressable, Text, View } from 'react-native';
+import HeaderBar from './HeaderBar';
+
+const Cards = () => {
+  return (
+    <View>
+      <HeaderBar title='Cards' />
+      <Text>Cards</Text>
+    </View>
+  )
+}
+
+export default Cards
