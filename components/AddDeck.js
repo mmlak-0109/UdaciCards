6b0830368ff77e4a2ac4a538a1b48ca529685d2a
Matthew <mmlak_0109@yahoo.com>
2021-04-29 14:10:36 +0900

    Add HeaderBar

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Pressable, Text, TextInput, View } from 'react-native';
+import HeaderBar from './HeaderBar';
 
 const AddDeck = () => {
   return (
     <View>
+      <HeaderBar title='Add Deck'/>
       <TextInput placeholder='Enter Deck Name'/>
       <Pressable>
         <Text>Add Deck</Text>
