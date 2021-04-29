77f8eb443f836a3211f23122f46c96bf11cee2fc
Matthew <mmlak_0109@yahoo.com>
2021-04-29 23:49:33 +0900

    Add DeckNav to stack

--- components/StackNav.js
+++ components/StackNav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeNav from './HomeNav';
+import DeckNav from './DeckNav';
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ const StackNav = () => {
   return (
     <Stack.Navigator initialRouteName='Decks'>
       <Stack.Screen name='Decks' component={HomeNav} options={{headerShown: false}} />
+      <Stack.Screen name='Cards' component={DeckNav} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
