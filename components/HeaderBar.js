c68148de39349eab9990b5f446ab870a5f5f41bc
Matthew <mmlak_0109@yahoo.com>
2021-04-30 00:11:47 +0900

    Add back button to Cards header

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-const HeaderBar = ({title}) => {
+const HeaderBar = ({ title }) => {
   return (
     <View style={styles.container}>
+      {title === 'Cards'
+        ? <MaterialCommunityIcons name="chevron-left-circle" size={24} color="white" />
+        : <Text></Text>
+      }
       <Text style={styles.text}>{title}</Text>
+      <Text></Text>
     </View>
   )
 }
@@ -13,6 +19,9 @@ export default HeaderBar
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#00BCD4'    
   },
   text: {
