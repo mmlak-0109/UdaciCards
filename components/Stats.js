93bbd888b8242add587a52731f4865aea1ebdf1d
Matthew <mmlak_0109@yahoo.com>
2021-04-29 23:49:39 +0900

    Create skeleton component

--- components/Stats.js
+++ components/Stats.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import HeaderBar from './HeaderBar';
 
 const Stats = () => {
