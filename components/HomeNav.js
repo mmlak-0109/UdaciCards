8a040aff720195516b836b7b182d416627a0674f
Matthew <mmlak_0109@yahoo.com>
2021-04-30 23:52:23 +0900

    Add tabVarOptions and hard code icon colors

--- components/HomeNav.js
+++ components/HomeNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -10,27 +10,31 @@ import Stats from './Stats';
 const HomeTabs = createBottomTabNavigator();
 
 const screenOptions = ({route}) => ({
-  tabBarIcon: ({ focused, color, size }) => {
+  tabBarIcon: ({ focused, color }) => {
     let iconName;
 
     if (route.name === 'Home') {
       iconName = focused ? 'home' : 'home-outline'
-      return <Ionicons name={iconName} size={24} color="#00BCD4" />
+      return <Ionicons name={iconName} size={24} color={color} />
     }
     if (route.name === 'Add Deck') {
       iconName = focused ? 'plus-circle' : 'plus-circle-outline'
-      return <MaterialCommunityIcons name={iconName} size={24} color="#00BCD4" />
+      return <MaterialCommunityIcons name={iconName} size={24} color={color} />
     }
     if (route.name === 'Stats') {
       iconName = focused ? 'stats-chart' : 'stats-chart-outline'
-      return <Ionicons name={iconName} size={24} color="#00BCD4" />
+      return <Ionicons name={iconName} size={24} color={color} />
     }
+  },
+  tabBarOptions: {
+    activeTintColor: '#00BCD4',
+    inactiveTintColor: '#00BCD4'
   }
 })
 
 const HomeNav = () => {
   return (
-    <HomeTabs.Navigator initialRouteName='Home' screenOptions={screenOptions}>
+    <HomeTabs.Navigator initialRouteName='Home' screenOptions={screenOptions} >
       <HomeTabs.Screen name='Home' component={Decks} />
       <HomeTabs.Screen name='Add Deck' component={AddDeck} />
       <HomeTabs.Screen name='Stats' component={Stats} />
@@ -39,3 +43,9 @@ const HomeNav = () => {
 }
 
 export default HomeNav
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#00BCD4'
+  }
+})
