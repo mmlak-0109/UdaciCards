3cf0333ae17264e62c0c090b81c3a6d39306f499
Matthew <mmlak_0109@yahoo.com>
2021-04-30 23:51:37 +0900

    Create storage key and IDs for users and questions

--- utils/_DATA.js
+++ utils/_DATA.js
@@ -1,35 +1,38 @@
-const decks = {
-  '1' : {
-    id: '1',
+// TODO: Add storage key and create functions in api file to
+// handle data fetches, etc.
+
+export const DECKS_STORAGE_KEY = 'UdaciCards:decks'
+
+export const decks = {
+  'a1paw3arzvmsjzunbqldc8' : {
+    id: 'a1paw3arzvmsjzunbqldc8',
     title: 'React',
     dateCreated: new Date('04-21-2021'),
     cardCount: 2,
     questions: [
       {
-        id: 1,
+        id: 'ko2din042rzlbz32bbl2c',
         question: 'What is React?',
         answer: 'A library for managing user interfaces'
       },
       {
-        id: 2,
+        id: 'xknz00phtpdkncgudx712',
         question: 'Where do you make Ajax requests in React?',
         answer: 'The componentDidMount lifecycle event'
       }
     ]
   },
-  '2' : {
-    id: '2',
+  'fm71encsnjcqqpewbqrzz' : {
+    id: 'fm71encsnjcqqpewbqrzz',
     title: 'JavaScript',
     dateCreated: new Date('04-21-2021'),
     cardCount: 1,
     questions: [
       {
-        id: 1,
+        id: 'o5iv0kilqag7fkg51ges',
         question: 'What is a closure?',
         answer: 'The combination of a function and the lexical environment within which that function was declared.'
       }
     ]
   }
 }
-
-export default decks
