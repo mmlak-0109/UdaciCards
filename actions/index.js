bd3d2c4950afb7e179840dde2fb1fc3e3abd33c5
matthew <mmlak_0109@yahoo.com>
2021-05-01 22:56:34 +0900

    Create receiveDecks action

---
+++ actions/index.js
@@ -0,0 +1,8 @@
+export const RECEIVE_DECKS = 'RECEIVE_DECKS'
+export const ADD_DECK = 'ADD_DECK'
+export const ADD_CARD = 'ADD_CARD'
+
+export const receiveDecks = (decks) => ({
+    type: RECEIVE_DECKS,
+    decks
+})
