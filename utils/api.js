45e3ef3b4a1e83d4cd6457eab5fe5f3205f6b464
matthew <mmlak_0109@yahoo.com>
2021-05-02 23:10:40 +0900

    Remove console logs

--- utils/api.js
+++ utils/api.js
@@ -5,14 +5,12 @@ import {DECKS_STORAGE_KEY, decks } from './_DATA'
 export const initializeData = async () => {
   try {
     let data = await AsyncStorage.getItem(DECKS_STORAGE_KEY)
-    console.log('data:', data)
     if (data === null) {
       data = decks
       await AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
     }
 
     data = JSON.parse(data)
-    console.log('dataParsed:', data)
 
     return data
 
