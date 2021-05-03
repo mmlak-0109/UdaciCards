0de64538457311e3f5cfe755d862eed85a25b2bc
matthew <mmlak_0109@yahoo.com>
2021-05-03 23:54:39 +0900

    Added code to recevie route params and select deck

--- components/Cards.js
+++ components/Cards.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import HeaderBar from './HeaderBar';
 
-const Cards = ({ navigation }) => {
+const Cards = ({ route, navigation }) => {
+  const { id } = route.params
+  const deck = useSelector(state => state.decks[id])
   return (
     <View>
       <HeaderBar 
         title='Cards'
         handlePress={() => navigation.navigate('Home')} />
-      <Text>Cards</Text>
+      <Text>{JSON.stringify(deck)}</Text>
     </View>
   )
 }
