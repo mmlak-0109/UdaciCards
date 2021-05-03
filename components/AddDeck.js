ead12e644eae209bb1b4c7410f74cb05bdfb043c
matthew <mmlak_0109@yahoo.com>
2021-05-03 23:58:26 +0900

    Add local state, reset state func, and handle submit func

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Pressable, Text, TextInput, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { addNewDecks } from '../actions';
+import { saveNewDeck } from '../utils/api';
 import HeaderBar from './HeaderBar';
 
-const AddDeck = () => {
+const AddDeck = ({ navigation }) => {
+  const initialDeckName = ''
+  const [deckName, updateDeckName] = useState(initialDeckName)
+  const dispatch = useDispatch()
+
+  // How to send info to navigate and update deck name
+  const resetState = () => {
+    updateDeckName(initialDeckName)
+  }
+
+  const handleSubmit = () => {
+    saveNewDeck(deckName)
+      .then(newDeck => {
+        dispatch(addNewDecks(newDeck))
+        navigation.navigate('Cards', {
+          screen: 'Cards',
+          params: {id: newDeck.id}
+        })
+        resetState()
+      })
+  }
+
   return (
     <View>
       <HeaderBar title='Add Deck'/>
-      <TextInput placeholder='Enter Deck Name'/>
-      <Pressable>
+      <TextInput 
+        placeholder='Enter Deck Name'
+        value={deckName}
+        onChangeText={updateDeckName} />
+      <Pressable 
+        onPress={handleSubmit}
+      >
         <Text>Add Deck</Text>
       </Pressable>
     </View>
