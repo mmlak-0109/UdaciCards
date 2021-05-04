549b7263baff73a16e0ae309b01b9551849b1412
matthew <mmlak_0109@yahoo.com>
2021-05-04 15:30:07 +0900

    Add top card and list of questions/answers

--- components/Cards.js
+++ components/Cards.js
@@ -1,19 +1,79 @@
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import HeaderBar from './HeaderBar';
 
 const Cards = ({ route, navigation }) => {
   const { id } = route.params
   const deck = useSelector(state => state.decks[id])
+
+  const addEllipsis = (str) => {
+    return (
+      str.includes('?')
+        ? str
+        : `${str}...?`
+    )
+  }
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Pressable
+          style={styles.questionCard}
+          // onPress={handlePress}
+        >
+          <View>
+            <Text>{addEllipsis(item.question.slice(0, 30))}</Text>
+            <Text>{item.answer.slice(0, 30)}...</Text>
+          </View>
+          <View>
+            <Text style={{color: '#00BCD4'}}>
+              View/Edit
+            </Text>
+          </View>
+        </Pressable>
+      </View>
+    )
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <HeaderBar 
         title='Cards'
         handlePress={() => navigation.navigate('Home')} />
-      <Text>{JSON.stringify(deck)}</Text>
+      <View style={styles.topCard}>
+        <Text>Created: {deck.dateCreated}</Text>
+        <Text>{deck.cardCount} Cards</Text>
+      </View>
+      <FlatList 
+        data={deck.questions}
+        renderItem={renderItem}
+        keyExtractor={(question) => question.id}
+        style={styles.list}
+      />
     </View>
   )
 }
 
 export default Cards
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  topCard: {
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 30,
+  },
+  questionCard: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 15,
+  }
+})
