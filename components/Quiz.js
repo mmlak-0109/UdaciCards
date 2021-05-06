1942d76fbeebb60f795f18faf3401fc3bbc6cdbc
matthew <mmlak_0109@yahoo.com>
2021-05-06 23:47:40 +0900

    Add ProgressBar, QuizCard, and quit button

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import HeaderBar from './HeaderBar';
+import ProgressBar from './ProgressBar';
+import QuizCard from './QuizCard';
 
-const Quiz = () => {
+const Quiz = ({ navigation, id }) => {
+  const deck = useSelector(state => state.decks[id])
+
+  const [currentCard, updateCurrentCard] = useState(1)
+  const totalCards = deck.cardCount
+  const pctFinished = Math.round((currentCard/totalCards)*100)
+
+  const currentIndex = currentCard-1
+  const currentQuestion = deck.questions[currentIndex].question
+  const currentAnswer = deck.questions[currentIndex].answer
+
+
   return (
-    <View>
+    <View style={{justifyContent: 'space-between'}}>
       <HeaderBar title='Quiz'/>
-      <Text>Quiz</Text>
+      <ProgressBar 
+        currentCard={currentCard}
+        totalCards={totalCards}
+        pctFinished={pctFinished} 
+      />
+      <QuizCard
+        question={currentQuestion}
+        answer={currentAnswer}
+      />
+      <View>
+        <Pressable
+          onPress={() => setModalVisible(!modalVisible)}
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>Quit?</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
 
 export default Quiz
+
+const styles = StyleSheet.create({
+  btn: {
+  },
+  btnText: {
+    color: '#00BCD4',
+    textAlign: 'center'
+  },
+});
