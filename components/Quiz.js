34ea865a5634550c109198c90b6c2ae86a7e3a16
matthew <mmlak_0109@yahoo.com>
2021-05-07 23:53:26 +0900

    Add functons to flip cards, mark answers, and quit quiz

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { set } from 'react-native-reanimated';
 import { useSelector } from 'react-redux';
 import HeaderBar from './HeaderBar';
 import ProgressBar from './ProgressBar';
@@ -12,39 +13,74 @@ const Quiz = ({ navigation, id }) => {
   const totalCards = deck.cardCount
   const pctFinished = Math.round((currentCard/totalCards)*100)
 
-  const currentIndex = currentCard-1
-  const currentQuestion = deck.questions[currentIndex].question
-  const currentAnswer = deck.questions[currentIndex].answer
+  const [modalVisible, setModalVisible] = useState(true);
 
+  const onAnswerMark = () => {
+    updateCurrentCard(currentCard + 1)
+    setModalVisible(true)
+  }
 
-  return (
-    <View style={{justifyContent: 'space-between'}}>
-      <HeaderBar title='Quiz'/>
-      <ProgressBar 
-        currentCard={currentCard}
-        totalCards={totalCards}
-        pctFinished={pctFinished} 
-      />
-      <QuizCard
-        question={currentQuestion}
-        answer={currentAnswer}
-      />
-      <View>
+  const onQuit = () => {
+    navigation.navigate('Cards', {
+      screen: 'Cards',
+      params: {id}
+    })
+    updateCurrentCard(1)
+    setModalVisible(true)
+  }
+
+  if (currentCard <= totalCards) {
+    const currentIndex = currentCard-1
+    const currentQuestion = deck.questions[currentIndex].question
+    const currentAnswer = deck.questions[currentIndex].answer
+
+    return (
+      <View style={styles.container}>
+        <HeaderBar title='Quiz'/>
+        <ProgressBar 
+          currentCard={currentCard}
+          totalCards={totalCards}
+          pctFinished={pctFinished} 
+        />
+        <QuizCard
+          question={currentQuestion}
+          answer={currentAnswer}
+          modalVisible={modalVisible}
+          onCardFlip={setModalVisible}
+          onAnswerMark={onAnswerMark}
+        />
         <Pressable
-          onPress={() => setModalVisible(!modalVisible)}
+          onPress={onQuit}
           style={styles.btn}
         >
           <Text style={styles.btnText}>Quit?</Text>
         </Pressable>
       </View>
-    </View>
-  )
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Quiz Complete!</Text>
+        <Pressable
+          onPress={onQuit}
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>Exit</Text>
+        </Pressable>
+      </View>
+    )
+  }
 }
 
 export default Quiz
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   btn: {
+    marginTop: 30,
+    marginBottom: 20
   },
   btnText: {
     color: '#00BCD4',
