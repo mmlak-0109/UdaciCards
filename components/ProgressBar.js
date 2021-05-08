6744c0cffda01bc6131deaba621c59b90955d158
matthew <mmlak_0109@yahoo.com>
2021-05-08 22:49:05 +0900

    Comment out code yet to be implimented

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -2,29 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const ProgressBar = ({ currentCard, totalCards, pctFinished }) => {
-  const barContainer = {
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#e0e0de',
-    borderRadius: 50,
-  }
+  // const barContainer = {
+  //   height: '100%',
+  //   width: '100%',
+  //   backgroundColor: '#e0e0de',
+  //   borderRadius: 50,
+  // }
 
-  const optionOneBar = {
-    height: '100%',
-    width: `${pctFinished}%`,
-    backgroundColor: '#00b5d6',
-    borderRadius: 'inherit',
-    textAlign: 'right'
-  }
+  // const optionOneBar = {
+  //   height: '100%',
+  //   width: `${pctFinished}%`,
+  //   backgroundColor: '#00b5d6',
+  //   borderRadius: 'inherit',
+  //   textAlign: 'right'
+  // }
 
   return (
     <View style={{marginVertical: 15}}>
       <Text style={styles.text}>
         Card {currentCard}/{totalCards}
       </Text>
-      <View style={styles.barContainer}>
+      {/* <View style={styles.barContainer}>
         <View style={styles.optionOneBar}></View>
-      </View>
+      </View> */}
     </View>
   )
 }
@@ -36,17 +36,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20
   },
-  barContainer: {
-    // height: '100%',
-    // width: '100%',
-    backgroundColor: '#e0e0de',
-    borderRadius: 50,
-  },
-  optionOneBar: {
-    // height: '100%',
-    // width: `${pctFinished}%`,
-    backgroundColor: '#00b5d6',
-    borderRadius: 50,
-    textAlign: 'right'
-  }
+  // barContainer: {
+  //   // height: '100%',
+  //   // width: '100%',
+  //   backgroundColor: '#e0e0de',
+  //   borderRadius: 50,
+  // },
+  // optionOneBar: {
+  //   // height: '100%',
+  //   // width: `${pctFinished}%`,
+  //   backgroundColor: '#00b5d6',
+  //   borderRadius: 50,
+  //   textAlign: 'right'
+  // }
 })
