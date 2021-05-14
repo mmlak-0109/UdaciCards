ed24c989ddb63459591203af28492be5ad764165
Matthew Miller <mmlak_0109@yahoo.com>
2021-05-15 00:04:24 +0900

    feat: Recieve new props
    Revieve ref and onSubmitEditing prop to change text box focus on return

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { TextInput } from 'react-native';
 import { primary } from '../utils/colors';
 
-const InputBox = ({ placeholder, value, autoFocus, onChangeText, blurOnSubmit, onSubmitHeading, multiline }) => {
+const InputBox = ({ placeholder, value, refProp, autoFocus, blurOnSubmit, onChangeText, onSubmitEditing, multiline }) => {
   return (
     <TextInput 
       placeholder={placeholder}
       value={value}
       autoFocus={autoFocus}
+      ref={refProp}
       onChangeText={onChangeText}
       blurOnSubmit={blurOnSubmit}
-      onSubmitHeading={onSubmitHeading}
+      onSubmitEditing={onSubmitEditing}
       multiline={multiline}
       style={{
         marginHorizontal: 25,
