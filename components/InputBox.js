7267c7c38b3f8d634b45052e1e224398e1af53d7
Matthew Miller <mmlak_0109@yahoo.com>
2021-05-14 10:34:50 +0900

    feat: Pass more props for greater control
    Pass props to autoFocus, blurOnSubmit, onSubmitHeading, and multiline

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { TextInput } from 'react-native';
 import { primary } from '../utils/colors';
 
-const InputBox = ({ placeholder, value, onChangeText }) => {
+const InputBox = ({ placeholder, value, autoFocus, onChangeText, blurOnSubmit, onSubmitHeading, multiline }) => {
   return (
     <TextInput 
       placeholder={placeholder}
       value={value}
+      autoFocus={autoFocus}
       onChangeText={onChangeText}
-      multiline
+      blurOnSubmit={blurOnSubmit}
+      onSubmitHeading={onSubmitHeading}
+      multiline={multiline}
       style={{
         marginHorizontal: 25,
         marginBottom: 15,
